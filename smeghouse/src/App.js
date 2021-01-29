a54b04d4b60124cba7c3c7f02348a632c73e27da
Yuliya Gaspodarik <gaspod.yu@gmail.com>
2021-01-29 17:15:08 +0300

    Add footer component

--- smeghouse/src/App.js
+++ smeghouse/src/App.js
@@ -3,11 +3,13 @@ import React from "react";
 import './App.css';
 import {Header} from "./Header/Header";
 import {Main} from "./Main/Main"
+import {Footer} from "./Footer/Footer"
 function App() {
   return (
     <React.Fragment>
       <Header/>
       <Main/>
+      <Footer />
     </React.Fragment>
   );
 }
