5e91fb3e5371a40a87aed380092b3d390235d3ab
Yuliya Gaspodarik <gaspod.yu@gmail.com>
2021-01-29 03:30:55 +0300

    Maked header

--- smeghouse/src/Header/Header.js
+++ smeghouse/src/Header/Header.js
@@ -4,14 +4,21 @@ import React from "react";
 import "./Header.css";
 
 export const Header = () => (
-  <header>
+  <header className="header">
+    <p className="header__title"><span>Smeg</span>house</p>
     <nav>
-      <ul>
+      <ul className="header__menu">
         <li><Link to="/">Главная</Link></li>
         <li><Link to="/products">Каталог</Link></li>
-        <li><Link to="/login">Авторизоваться</Link></li>
-        <li><Link to="/favorite">Избранное</Link></li>
-        <li><Link to="/stock">Корзина</Link></li>
+        <li><label className="header__search">
+          <span></span>
+          <input placeholder="Search"/>
+        </label></li>
+      </ul>
+      <ul className="header__buttons">
+        <li><Link to="/login" className="fa fa-user"/></li>
+        <li><Link to="/favorite" className="fa fa-heart"/></li>
+        <li><Link to="/stock" className="fa fa-shopping-cart"/></li>
       </ul>
     </nav>
   </header>
