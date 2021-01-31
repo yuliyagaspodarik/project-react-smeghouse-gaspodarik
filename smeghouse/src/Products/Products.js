da06281a8395e41ebec836c4127927194fd97779
Yuliya Gaspodarik <gaspod.yu@gmail.com>
2021-02-01 02:44:35 +0300

    Add lists of categories and products

--- smeghouse/src/Products/Products.js
+++ smeghouse/src/Products/Products.js
@@ -7,32 +7,40 @@ import {
   useRouteMatch
 } from "react-router-dom";
 
+import "./Products.css";
 import {Product} from "../Product/Product.js";
 
 export const Products = (props) => {
   console.log('products', props);
-
   let match = useRouteMatch();
 
   return (
-    <div>
-      <h2>Products</h2>
-      <ul>
-        <li>
-          <Link to={`${match.url}/1`}>Product 1</Link>
-        </li>
-        <li>
-          <Link to={`${match.url}/2`}>Product 2</Link>
-        </li>
-      </ul>
+    <div className="catalog">
+      <div className="catalog__categories">
+        <h3>Категории</h3>
+        <ul>
+          {props.filters.map((filter) => (
+            <li key={filter}><label className="catalog__categories-item"><input type="checkbox" name={filter}/>{filter}
+            </label></li>))}
+        </ul>
+      </div>
+      <div className="catalog__products">
+        <ul>
+          {props.products.map((product) => (
+            <li className="catalog__item" key={product.id}>
+              <Link to={`${match.url}/${product.id}`}>{product.name}</Link>
+            </li>))}
+        </ul>
 
-      <Switch>
-        <Route path={`${match.path}/:productId`}>
-          <Product/>
-        </Route>
-        <Route path={match.path}>
-          <h3>Please use correct product</h3>
-        </Route>
-      </Switch>
-    </div>)
+        <Switch>
+          <Route path={`${match.path}/:productId`}>
+            <Product/>
+          </Route>
+          <Route path={match.path}>
+            <h3>Please use correct product</h3>
+          </Route>
+        </Switch>
+      </div>
+    </div>
+  )
 };
