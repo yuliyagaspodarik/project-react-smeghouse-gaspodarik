dc853d3c7775ace56781765f75e14fa7f3d9a87a
Yuliya Gaspodarik <gaspod.yu@gmail.com>
2021-02-01 01:01:52 +0300

    Make general function for requests

--- smeghouse/src/api/requests.js
+++ smeghouse/src/api/requests.js
@@ -1,43 +1,15 @@
 const apiRoot = "http://localhost:5000";
 
-export async function getAllProducts() {
-  const allProductsResponse = await fetch(`${apiRoot}/products/`);
+export async function getData(request) {
+  const dataResponse = await fetch(`${apiRoot}/${request}/`);
 
-  if (!allProductsResponse.ok) {
-    console.log(`Error with status ${allProductsResponse.status}`);
+  if (!dataResponse.ok) {
+    console.log(`Error with status ${dataResponse.status}`);
     return;
   }
 
-  const products = await allProductsResponse.json();
-  console.log('request products', products);
+  const data = await dataResponse.json();
+  console.log('request', data);
 
-  return products;
-}
-
-export async function getAllFilters() {
-  const allFiltersResponse = await fetch(`${apiRoot}/filters/`);
-
-  if (!allFiltersResponse.ok) {
-    console.log(`Error with status ${allFiltersResponse.status}`);
-    return;
-  }
-
-  const filters = await allFiltersResponse.json();
-  console.log('request filters', filters);
-
-  return filters;
-}
-
-export async function getContacts() {
-  const contactsResponse = await fetch(`${apiRoot}/contacts/`);
-
-  if (!contactsResponse.ok) {
-    console.log(`Error with status ${contactsResponse.status}`);
-    return;
-  }
-
-  const contacts = await contactsResponse.json();
-  console.log('request filters', contacts);
-
-  return contacts;
+  return data;
 }
--- smeghouse/src/index.js
+++ smeghouse/src/index.js
@@ -1,30 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {BrowserRouter as Router} from "react-router-dom";
-import {createStore} from "redux";
-import {Provider} from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 import reducer from "./reducers/reducer";
 import App from "./App";
 import "./index.css";
-import {getAllProducts, getAllFilters, getContacts} from "./api/requests";
+import { getData } from "./api/requests";
 
 const store = createStore(reducer);
 
 const initStateData = async () => {
-  /*const products = await getAllProducts();
-  const filters = await getAllFilters();
-  const contacts = await getContacts();*/
   const data = {
-    products: await getAllProducts(),
-    filters: await getAllFilters(),
-    contacts: await getContacts()
+    products: await getData("products"),
+    filters: await getData("filters"),
+    contacts: await getData("contacts")
   };
 
   store.dispatch({
     type: "SET_STATE",
     payload: {
       data: data
-      /*data: {products: products, filters: filters, contacts: contacts}*/
     }
   });
 };
