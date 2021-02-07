997a23fc0605ad37ac09dd11c136ee922512228e
Yuliya Gaspodarik <gaspod.yu@gmail.com>
2021-02-07 04:45:12 +0300

    Finished notFound component

--- smeghouse/src/Header/Header.js
+++ smeghouse/src/Header/Header.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
 import "./Header.css";
 import { Search } from "../Search/Search";
 
--- smeghouse/src/NotFound/NotFound.js
+++ smeghouse/src/NotFound/NotFound.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import "./NotFound.css";
 
 export const NotFound = () => (
   <main>
-    <h1>page not found</h1>
+    <div className="notfound">
+      <h2>Error 404</h2>
+      <h3>page not found</h3>
+      <p className="notfound__message">
+        Кто-то потерялся: или вы, или мы
+      </p>
+      <p className=" notfound__link">
+        Вернуться на <Link to="/" title=" ссылка для перехода">" Главную"</Link>
+      </p>
+    </div>
   </main>
 );
