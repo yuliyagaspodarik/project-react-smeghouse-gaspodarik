e16225647f7cee7f0923a010551152b6f7a59d34
Yuliya Gaspodarik <gaspod.yu@gmail.com>
2021-02-09 03:15:47 +0300

    Add form with bootstrap

--- smeghouse/src/components/Login/Login.js
+++ smeghouse/src/components/Login/Login.js
@@ -1,84 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 
 import "./Login.css";
 
-class Login extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      login: "",
-      password: "",
-      email: ""
-    };
+function Login() {
+  const [validated, setValidated] = useState(false);
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
 
-  handleChange(event) {
-    const target = event.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    const name = target.name;
+    setValidated(true);
+  };
 
-    this.setState({
-      [name]: value
-    });
-  }
-
-  handleSubmit(event) {
-    alert('Отправленное имя: ' + this.state.value);
-    event.preventDefault();
-  }
-
-  render() {
-    return (
-      <main>
-        <div className="auth">
-          <form className="auth__form" onSubmit={this.handleSubmit}>
-            <label>
-              <input
-                className="auth__form-input"
-                type="text"
-                name="login"
-                placeholder="Логин"
-              />
-            </label>
-            <label>
-              <input
-                className="auth__form-input"
-                type="password"
-                name="password"
-                placeholder="Пароль"
-              />
-            </label>
-            <label>
-              <input
-                className="auth__form-input"
+  return (
+    <main>
+      <div className="auth">
+        <Form noValidate validated={validated} onSubmit={handleSubmit}>
+          <Form.Row>
+            <Form.Group as={Col} md="4" controlId="validationCustom01">
+              <Form.Label>Логин</Form.Label>
+              <Form.Control
+                required
                 type="email"
-                name="email"
-                placeholder="E-mail"
+                placeholder="Введите email"
               />
-            </label>
-            <label>
-              <input
-                className="auth__form-input"
-                type="checkbox"
-                name="check"
+              <Form.Control.Feedback>Отлично!</Form.Control.Feedback>
+              <Form.Control.Feedback type="invalid">
+                Введите корректный email
+              </Form.Control.Feedback>
+            </Form.Group>
+          </Form.Row>
+          <Form.Row>
+            <Form.Group as={Col} md="4" controlId="validationCustom02">
+              <Form.Label>Пароль</Form.Label>
+              <Form.Control
+                required
+                type="password"
+                placeholder="Введите пароль"
               />
-              Согласен с условиями правил пользования торговой площадки
-            </label>
-            <div>
-              <button type="submit" className="auth__form-button">Войти</button>
-              <button className="auth__form-button" onChange={this.handleSubmit}>Зарегистрироваться</button>
-            </div>
-            <div>
-              <button className="auth__form-button auth__form-button-exit" onChange={this.handleSubmit}>Выйти</button>
-            </div>
-          </form>
-        </div>
-      </main>
-    );
-  }
+              <Form.Control.Feedback>Отлично!</Form.Control.Feedback>
+              <Form.Control.Feedback type="invalid">
+                Введите надежный пароль
+              </Form.Control.Feedback>
+            </Form.Group>
+          </Form.Row>
+          <Button type="submit">Авторизоваться</Button>
+        </Form>
+      </div>
+    </main>
+  );
 }
 
 export default Login;
